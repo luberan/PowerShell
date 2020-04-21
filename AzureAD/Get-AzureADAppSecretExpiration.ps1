013d55ccbf0e849d6298c04ee4c18f852bc3ed7d
Lukas Beran <35958932+luberan@users.noreply.github.com>
2020-04-21 12:55:51 +0200

    Create Get-AzureADAppSecretExpiration.ps1

---
+++ AzureAD/Get-AzureADAppSecretExpiration.ps1
@@ -0,0 +1,43 @@
+# Requires Azure AD PowerShell Module
+#Prompts user to login using Azure Credentials
+
+Connect-AzureAD
+$results = @()
+Get-AzureADApplication -All $true | %{  
+                             $app = $_
+
+                             $owner = Get-AzureADApplicationOwner -ObjectId $_.ObjectID -Top 1
+
+                             $app.PasswordCredentials | 
+                                %{ 
+                                    $results += [PSCustomObject] @{
+                                            CredentialType = "PasswordCredentials"
+                                            DisplayName = $app.DisplayName; 
+                                            AppId = $app.AppId;
+                                            ExpiryDate = $_.EndDate;
+                                            StartDate = $_.StartDate;
+                                            KeyID = $_.KeyId;
+                                            Type = 'NA';
+                                            Usage = 'NA';
+                                            Owners = $owner.UserPrincipalName;
+                                        }
+                                 } 
+                                  
+                             $app.KeyCredentials | 
+                                %{ 
+                                    $results += [PSCustomObject] @{
+                                            CredentialType = "KeyCredentials"                                        
+                                            DisplayName = $app.DisplayName; 
+                                            ExpiryDate = $_.EndDate;
+                                            StartDate = $_.StartDate;
+                                            KeyID = $_.KeyId;
+                                            Type = $_.Type;
+                                            Usage = $_.Usage;
+                                            Owners = $owner.UserPrincipalName;
+                                        }
+                                 }                            
+                          }
+$results | FT -AutoSize 
+
+# Optionally export to a CSV file
+#$results | Export-Csv -Path "AppsInventory.csv" -NoTypeInformation 
