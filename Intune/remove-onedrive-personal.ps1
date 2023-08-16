5490d8cf039d97114bb1d7cb2dae6afd09b1fb60
Lukas Beran <35958932+luberan@users.noreply.github.com>
2023-08-16 09:13:28 +0200

    Add files via upload
    Based on https://www.nielskok.tech/intune/remove-onedrive-personal-icon/

---
+++ Intune/remove-onedrive-personal.ps1
@@ -0,0 +1,15 @@
+﻿Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\RemoveOneDrivePersonalIcon.log"
+$OneDriverPersonalIcon = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -ErrorAction SilentlyContinue
+If ($OneDriverPersonalIcon){
+    try {
+        Get-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" | Remove-Item -Force -Verbose
+        Write-Output "Remove OneDrive Personal Icon"
+    }
+    catch {
+        Write-Error "Error: $($_.Exception.Message)"
+    }
+}
+else {
+    Write-Output "OneDrive Personal Icon not found"
+}
+Stop-Transcript
